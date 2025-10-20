a8c0b359179be2faf3a5319bab5b54137c96cb97
Bengin05 <uzunbengin9@gmail.com>
2025-10-20 14:07:53 +0000

    adding script 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+# include  <stdio.h>
+
+ /** 
+* main - Point d'entrée 
+* Retour : Toujours 0 (Succès) 
+*/ 
+
+int  main ( void )
+ { 
+ puts ( "\"Programmer, c'est comme construire un puzzle multilingue" ); 
+
+  return ( 0 ); 
+}
