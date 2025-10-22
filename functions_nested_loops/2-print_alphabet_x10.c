a8587500e57fb651b7b8e6e4414e8e47788b31cf
Bengin05 <uzunbengin9@gmail.com>
2025-10-22 09:58:42 +0000

    adding program 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* print_alphabet -  Prints 10 times the alphabet, in lowercase, followed by a new line
+* print_alphabet_x10 -  prints 10 times the alphabet, in lowercase, followed by a new line
 *
 * Return: void
 */
