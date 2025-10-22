0054e66014403a981506433df1e93530bfe6e55c
Bengin05 <uzunbengin9@gmail.com>
2025-10-22 08:43:04 +0000

    adding program 0

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
-#endif
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+int _putchar(char c);
+
+#endif /* MAIN_H */
