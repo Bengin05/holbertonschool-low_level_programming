bdadc6411b21ee00f70cc19e3aa505f396055f1e
Bengin05 <uzunbengin9@gmail.com>
2025-10-22 09:09:07 +0000

    adding program 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
-* main - Prints the alphabet, in lowercase, followed by a new line
+* main - Prints the alphabet, in lowercase followed by a new line
 *
 * Return: Always 0 (Success)
 */
