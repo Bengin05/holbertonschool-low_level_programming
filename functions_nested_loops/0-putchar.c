68dcd124354e07d9e1ce71ee0682c0ed8139bc34
Bengin05 <uzunbengin9@gmail.com>
2025-10-22 08:13:32 +0000

    adding program 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* mains - Prints _putchar, followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+    _putchar("_putchar, \n");
+    return (0);
+}
