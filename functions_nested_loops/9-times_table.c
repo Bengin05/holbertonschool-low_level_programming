d8540f33418ce5a0199a1b4d6b75f009a72cd72f
Bengin05 <uzunbengin9@gmail.com>
2025-10-23 13:54:53 +0000

    adding program 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,8 +9,10 @@ void times_table(void)
     int n, i;
 
     for ( n = 0; n < 10; n++)
+        if ( n < 10 )
 
     for ( i = 0; i< 10; i++ )
+        if ( i < 10 )
 
      _putchar("\n '%d:'\n", n);
      _putchar("\n '%d:'\n", i);
