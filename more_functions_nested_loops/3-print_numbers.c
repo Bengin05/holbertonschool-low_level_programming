ba80d908d029693de0ebb1067db6bad417a7dd12
Bengin05 <uzunbengin9@gmail.com>
2025-10-27 10:11:34 +0000

    adding program 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_numbers - Prints the numbers from 0 to 9
@@ -8,9 +7,11 @@
  */
 void print_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
-	putchar(c);
-	_putchar('\n');
+	    for (c = '0'; c <= '9'; c++)
+	{
+        _putchar(c + '0');
+    }    
+    _putchar('\n');
 }
