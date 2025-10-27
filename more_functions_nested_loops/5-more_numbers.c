7dcbed7a9f18937116745523b82677eb3058c1c8
Bengin05 <uzunbengin9@gmail.com>
2025-10-27 11:21:08 +0000

    adding program 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,16 @@
 */
 void more_numbers(void)
 {
-    int n;
-    int m;
+	int n, m;
 
-    for (m = 0; m < 10; m++)
-    {
-        for (n = 0; n <= 14; n++)
-        {
-            if (n >= 10)
-            _putchar((n / 10) + '0');
-            _putchar((n % 10) + '0');
-        }
-        _putchar('\n');
-    }
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
