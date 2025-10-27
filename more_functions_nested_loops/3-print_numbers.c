bc858eba3de6088a14f3fe10dae80accf9271db0
Bengin05 <uzunbengin9@gmail.com>
2025-10-27 10:53:30 +0000

    adding program 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* print_numbers - Write a function that prints the numbers, from 0 to 9
+*print_numbers - Write a function that prints the numbers, from 0 to 9
 *                 followed by a new line
-* 
-* Return: Always 0 (Success) 
+*
+* Return: Always 0 (Success)
 */
 void print_numbers(void)
 {
