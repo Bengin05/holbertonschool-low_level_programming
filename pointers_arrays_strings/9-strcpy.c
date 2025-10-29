4e4c6f1b42352a9f1f4c4e657f6b0ca04f6c757c
Bengin05 <uzunbengin9@gmail.com>
2025-10-29 15:33:24 +0000

    adding program 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copie la chaîne pointée par src, y compris l'octet nul de fin,
+ * dans le buffer pointé par dest
+ * @dest: pointeur vers le buffer de destination
+ * @src: pointeur vers la chaîne source à copier
+ *
+ * Return: le pointeur vers dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+    int i = 0;
+
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
