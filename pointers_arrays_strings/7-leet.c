472bc3150c7aeaa8f4ddddaf5533a9bc82ff3aac
Bengin05 <uzunbengin9@gmail.com>
2025-10-31 21:21:23 +0000

    adding program 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
-/* 
- * Write a function that encodes a string into 1337.
- * 
- * Letters a and A should be replaced by 4
- * Letters e and E should be replaced by 3
- * Letters o and O should be replaced by 0
- * Letters t and T should be replaced by 7
- * Letters l and L should be replaced by 1
+/**
+ * leet - encodes a string into 1337
+ * @str: string to encode
  *
- * Prototype: char *leet(char *);
+ * Description:
+ * This function replaces specific letters with numbers to form
+ * the "1337" encoding style:
+ *  - a and A are replaced by 4
+ *  - e and E are replaced by 3
+ *  - o and O are replaced by 0
+ *  - t and T are replaced by 7
+ *  - l and L are replaced by 1
  *
- * You can only use one if in your code
- * You can only use two loops in your code
- * The use of case-selection statements is not allowed
- * You are not allowed to use any ternary operation
+ * Return: pointer to the modified string
+ *
+ * Note:
+ * Only one if statement and two loops are allowed.
+ * No use of case-selection statements.
+ * No ternary operations.
  */
-
 char *leet(char *str)
 {
 	int i = 0, j;
