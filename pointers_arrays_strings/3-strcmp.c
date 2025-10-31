f5610ab15384041cb84e2ce0d67bc7fb489f0814
Bengin05 <uzunbengin9@gmail.com>
2025-10-31 10:59:33 +0000

    adding program 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,24 +13,14 @@
  * Return: an integer indicating the result of the comparison
  */
 int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
-		int i = 0;
+	int i = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
 		i++;
 	}
-	if (s1[i] > s2[j])
-	{
-		return (s1[i] - s2[i]);
-	}
-	else if (s1[i] == s2[i])
-	{
-		return (0);
-	}
-	else
-	{
-		return (s2[i] - s1[i]);
-	}
-	
+
+	return (s1[i] - s2[i]);
 }
