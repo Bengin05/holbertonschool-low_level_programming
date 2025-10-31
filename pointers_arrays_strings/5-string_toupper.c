ef3197edd4e4ed24572e8b8fec6ef6c45c9c7034
Bengin05 <uzunbengin9@gmail.com>
2025-10-31 15:18:29 +0000

    adding program 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -15,13 +15,12 @@ char *string_toupper(char *str)
 	int i = 0;
 
 	while (str[i] != '\0')
-	
 	{
 		if (str[i] >= 'a' && str[i] <='z')
-	{
-		str[i] = str[i] - 32;
+		{
+			str[i] = str[i] - 32;
+		}
 		i++;
 	}
-	}
-	return str;
+	return (str);
 }
