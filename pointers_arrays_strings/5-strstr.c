29e8d5c87f7ddc02ace536524537c27e3200f7a2
Bengin05 <uzunbengin9@gmail.com>
2025-11-03 14:06:51 +0000

    adding betty style

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,15 +2,18 @@
 #include <stddef.h>
 
 /*
- * _strstr - localise la première occurrence
- *			de la sous-chaîne 'needle' dans la chaîne 'haystack'
- * @haystack : la chaîne dans laquelle effectuer la recherche
- * @needle : la sous-chaîne à rechercher
- * Description : Cette fonction recherche la première occurrence de la sous-chaîne 'needle'
- * 				dans la chaîne 'haystack'.
- *				La comparaison n'inclut pas les caractères nuls terminaux ('\0').
- * Retour : un pointeur vers le début de la sous-chaîne trouvée dans 'haystack',
- * ou NULL si 'needle' n'est pas trouvée.
+ * _strstr - localise la première occurrence de la sous-chaîne 'needle'
+ *            dans la chaîne 'haystack'
+ * @haystack: la chaîne dans laquelle effectuer la recherche
+ * @needle: la sous-chaîne à rechercher
+ *
+ * Description: Cette fonction recherche la première occurrence de la
+ *              sous-chaîne 'needle' dans la chaîne 'haystack'. La
+ *              comparaison n'inclut pas les caractères nuls terminaux
+ *              ('\0').
+ *
+ * Return: un pointeur vers le début de la sous-chaîne trouvée dans
+ *         'haystack', ou NULL si 'needle' n'est pas trouvée.
  */
 char *_strstr(char *haystack, char *needle)
 {
