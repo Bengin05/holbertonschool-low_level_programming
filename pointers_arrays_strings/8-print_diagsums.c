26eb13ac9d9ae4a7282c0872d225392b4d45bd72
Bengin05 <uzunbengin9@gmail.com>
2025-11-03 14:47:06 +0000

    adding program 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Affiche la somme des deux diagonales principales
+ *                  d'une matrice carrée d'entiers.
+ * @a: Pointeur vers le premier élément de la matrice (traitée comme un int *).
+ * @size: Taille (nombre de lignes et de colonnes) de la matrice carrée.
+ *
+ * Return: Rien.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - 1 - i)];
+	}
+	print_number(sum1);
+	_putchar(' ');
+	print_number(sum2);
+	_putchar('\n');
+}
