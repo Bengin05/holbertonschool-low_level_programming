5b7db87f71dc79cf7f3013abdfc88073f93955b2
Bengin05 <uzunbengin9@gmail.com>
2025-11-04 15:27:04 +0000

    adding program 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_helper - fonction récursive qui cherche la racine carrée naturelle
- * @n: nombre dont on cherche la racine
- * @i: valeur testée comme racine potentielle
- * Return: la racine carrée naturelle, ou -1 si elle n'existe pas
- */
-int _sqrt_helper(int n, int i)
-{
-	if (i * i == n)
-		return (i);
-	else if (i * i > n)
-		return (-1);
-	else
-		return (_sqrt_helper(n, i + 1));
-}
-
-/**
  * _sqrt_recursion - retourne la racine carrée naturelle d’un nombre
  * @n: entier dont on veut la racine carrée
  * Return: la racine carrée naturelle de n, ou -1 si elle n’existe pas
@@ -29,4 +13,24 @@ int _sqrt_recursion(int n)
 		return (n);
 	else
 		return (_sqrt_helper(n, 1));
+}
+
+/**
+ * _sqrt_helper - fonction auxiliaire récursive pour trouver la racine carrée
+ * @n: nombre dont on cherche la racine carrée
+ * @i: nombre testé comme racine possible
+ *
+ * Cette fonction teste successivement les valeurs de i (1, 2, 3, …)
+ * jusqu’à ce que i * i == n (racine trouvée) ou que i * i > n (pas de racine).
+ *
+ * Return: la racine carrée naturelle de n, ou -1 si elle n'existe pas
+ */
+int _sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	else
+		return (_sqrt_helper(n, i + 1));
 }
