9b0d558ba790bbf8ee33cb0e7e4d35a989e3e049
Bengin05 <uzunbengin9@gmail.com>
2025-11-04 13:48:19 +0000

    adding program 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calcule la longueur d'une chaîne de caractères
+ * @s: pointeur vers la chaîne dont on veut connaître la longueur
+ *
+ * Cette fonction utilise la récursion pour compter le nombre de caractères
+ * dans une chaîne. Elle incrémente le compteur à chaque appel jusqu’à atteindre
+ * le caractère nul '\0', qui marque la fin de la chaîne.
+ *
+ * Return: la longueur de la chaîne de caractères
+ */
+int _strlen_recursion(char *s)
+{
+	int longeur;
+	
+	if (*s != '\0')
+	{
+		longeur = 1 + _strlen_recursion(s + 1);
+	}
+	return (longeur);
+}
