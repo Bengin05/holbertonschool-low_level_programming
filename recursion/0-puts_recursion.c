7abbe2a66a3636a91d36c57ffeefb51e720ad325
Bengin05 <uzunbengin9@gmail.com>
2025-11-04 09:34:53 +0000

    adding README.md , main.h and program 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - affiche une chaîne de caractères, suivie d'un saut de ligne
+ * @s: pointeur vers la chaîne à afficher
+ *
+ * Cette fonction utilise la récursion pour parcourir la chaîne de caractères
+ * jusqu’à la fin ('\0'). À chaque appel, elle affiche le caractère courant.
+ * Quand le caractère nul est atteint, elle affiche un saut de ligne et arrête.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
