d64a36d981c72a0d5c59b997260e029506444eb5
Bengin05 <uzunbengin9@gmail.com>
2025-11-04 14:06:33 +0000

    adding program 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * factorial - calcule la factorielle d'un nombre entier
+ * @n: entier dont on veut calculer la factorielle
+ *
+ * Cette fonction utilise la récursion pour calculer n! (n factorielle).
+ * - Si n est inférieur à 0, la fonction retourne -1 pour indiquer une erreur.
+ * - La factorielle de 0 est définie comme 1.
+ *
+ * Return: la factorielle de n, ou -1 si n < 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return(-1);
+	}
+	else if (n == 0)
+	{
+		return(1);
+	}
+	else
+	{
+		return(n * factorial(n - 1));
+	}
+}
