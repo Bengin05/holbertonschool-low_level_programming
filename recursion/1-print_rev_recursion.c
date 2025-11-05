6107cbb98cba298bea4c89a01e8490a21caceb9e
Bengin05 <uzunbengin9@gmail.com>
2025-11-05 09:10:28 +0000

    adding program 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - affiche une chaîne de caractères en ordre inverse
- * @s: pointeur vers la chaîne à afficher
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: Pointer to the string to print
  *
- * Cette fonction utilise la récursion pour parcourir la chaîne jusqu’à la fin,
- * puis affiche les caractères dans l’ordre inverse lors du retour des appels
- * récursifs. Elle n’utilise aucune boucle.
+ * Return: Nothing
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
+		return;
+
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
