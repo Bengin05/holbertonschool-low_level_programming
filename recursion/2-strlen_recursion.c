f52040f6622fb5c38d43f97ae4d5f57c1c711404
Bengin05 <uzunbengin9@gmail.com>
2025-11-05 09:47:12 +0000

    adding program 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -12,11 +12,7 @@
  */
 int _strlen_recursion(char *s)
 {
-	int longeur;
-	
-	if (*s != '\0')
-	{
-		longeur = 1 + _strlen_recursion(s + 1);
-	}
-	return (longeur);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s +1));
 }
