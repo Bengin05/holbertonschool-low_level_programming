e930cc98394cb4ac8a256a1b9a3bc36071a9199c
Bengin05 <uzunbengin9@gmail.com>
2025-11-05 14:46:18 +0000

    adding program 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,90 +9,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	
-	if (argc != 3)
+	int i, sum = 0;
+
+	if (argc == 1)
 	{
-		char *msg = "Error\n";
-		int i = 0;
-
-		while (msg[i])
-		{
-			_putchar(msg[i]);
-			i++;
-		}
-		return (1);
-	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 + num2;
-
-	print_number(result);
-	_putchar('\n');
-
-	return (0);
-}
-
-/**
- * _atoi - convertit une chaîne de chiffres en entier
- * @s: chaîne à convertir
- *
- * Return: entier correspondant
- */
-int _atoi(char *s)
-{
-	int sign = 1, num = 0;
-
-	if (*s == '-')
-	{
-		sign = -1;
-		s++;
-	}
-
-	while (*s)
-	{
-		num = num * 10 + (*s - '0');
-		s++;
-	}
-
-	return (num * sign);
-}
-
-/**
- * is_digit - vérifie si une chaîne ne contient que des chiffres
- * @s: chaîne à vérifier
- *
- * Return: 1 si la chaîne est composée uniquement de chiffres, 0 sinon
- */
-int is_digit(char *s)
-{
-	if (*s == '\0')
-	{
-		return (1);
-	}
-	
-	if (*s < '0' || *s > '9')
-	{
+		_putchar('0');
+		_putchar('\n');
 		return (0);
 	}
-	return (is_digit(s + 1));
-}
 
-/**
- * print_number - affiche un entier avec _putchar
- * @n: entier à afficher
- *
- * Return: rien
- */
-void print_number(int n)
-{
-	if (n < 0)
+	for (i = 1; i < argc; i++)
 	{
-		_putchar('-');
-		n = -n;
+		if (!is_digit(argv[i]))
+		{
+			char *msg = "Error\n";
+			int j = 0;
+
+			while (msg[j])
+			{
+				_putchar(msg[j]);
+				j++;
+			}
+			return (1);
+		}
+		sum += _atoi(argv[i]);
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+
+	print_number(sum);
+	_putchar('\n');
+	return (0);
 }
