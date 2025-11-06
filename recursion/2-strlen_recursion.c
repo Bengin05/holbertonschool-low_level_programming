97ad41b6a6e6358b4f9a93c44f62b8fd03428127
Bengin05 <uzunbengin9@gmail.com>
2025-11-06 09:27:36 +0000

    adding betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,7 +5,8 @@
  * @s: pointeur vers la chaîne dont on veut connaître la longueur
  *
  * Cette fonction utilise la récursion pour compter le nombre de caractères
- * dans une chaîne. Elle incrémente le compteur à chaque appel jusqu’à atteindre
+ * dans une chaîne.
+ * Elle incrémente le compteur à chaque appel jusqu’à atteindre
  * le caractère nul '\0', qui marque la fin de la chaîne.
  *
  * Return: la longueur de la chaîne de caractères
@@ -14,5 +15,5 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s +1));
+	return (1 + _strlen_recursion(s + 1));
 }
