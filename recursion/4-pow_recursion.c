562cf9b8a6298748859308dffee9d04608f2f724
Bengin05 <uzunbengin9@gmail.com>
2025-11-06 13:05:28 +0000

    adding betty

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -6,7 +6,7 @@
  * @y: exposant
  *
  * Cette fonction utilise la récursion pour calculer x^y.
- * - Si y est inférieur à 0, la fonction retourne -1 pour indiquer une erreur.
+ * - Si y est inférieur à 0, la fonction retourne -1 pour indiquer une erreur
  * - Si y est égal à 0, la fonction retourne 1 (cas de base).
  *
  * Return: x élevé à la puissance y, ou -1 si y < 0
@@ -15,14 +15,14 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		return(-1);
+		return (-1);
 	}
 	else if (y == 0)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return x * _pow_recursion(x, y - 1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
