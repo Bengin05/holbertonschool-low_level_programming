e5dc142da8b2ae7cd115ec4f8b5e01c893126ee9
Bengin05 <uzunbengin9@gmail.com>
2025-11-06 09:29:55 +0000

    adding betty

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -5,7 +5,7 @@
  * @n: entier dont on veut calculer la factorielle
  *
  * Cette fonction utilise la récursion pour calculer n! (n factorielle).
- * - Si n est inférieur à 0, la fonction retourne -1 pour indiquer une erreur.
+ * - Si n est inférieur à 0, la fonction retourne -1 pour indiquer une erreur
  * - La factorielle de 0 est définie comme 1.
  *
  * Return: la factorielle de n, ou -1 si n < 0
@@ -14,12 +14,11 @@ int factorial(int n)
 {
 	if (n < 0)
 	{
-		return(-1);
+		return (-1);
 	}
 	else if (n == 0)
 	{
-		return(1);
+		return (1);
 	}
-		return(n * factorial(n - 1));
-	
+		return (n * factorial(n - 1));
 }
