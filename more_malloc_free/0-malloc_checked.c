aa19794b171978aeccffed618c5c522c149fe835
Bengin05 <uzunbengin9@gmail.com>
2025-11-12 11:49:11 +0000

    adding the file and program 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Alloue de la mémoire en utilisant malloc
+ * @b: la taille en octets à allouer
+ *
+ * Description :
+ * Cette fonction tente d'allouer un bloc de mémoire de taille 'b' octets
+ * en utilisant malloc. Si l'allocation échoue (malloc renvoie NULL),
+ * le programme se termine immédiatement avec le code d’état 98.
+ *
+ * Return: un pointeur vers la mémoire allouée si le succès,
+ *         ne retourne rien si l’allocation échoue (le programme quitte).
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
