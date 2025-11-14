5c50e084885e4b71640eff0736401f410f93264b
Bengin05 <uzunbengin9@gmail.com>
2025-11-14 15:26:40 +0000

    adding function 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -7,10 +6,12 @@
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
+ *
+ * Description: If d is not NULL, assigns values to its fields.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
