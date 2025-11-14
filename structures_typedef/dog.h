02b7b7d287d34818e998019f8b52c14bae76d54a
Bengin05 <uzunbengin9@gmail.com>
2025-11-14 08:47:44 +0000

    adding

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,11 +9,11 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 #endif
