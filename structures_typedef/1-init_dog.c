53e8875ec9844a0423a839ce45ddb791f8eedc09
Bengin05 <uzunbengin9@gmail.com>
2025-11-14 15:38:44 +0000

    adding function 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -6,6 +7,8 @@
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
+ *
+ * Return: nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
