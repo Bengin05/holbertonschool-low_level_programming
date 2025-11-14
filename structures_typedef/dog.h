574bcb6d13172fcf22431c7b5c6a2c221e14644d
Bengin05 <uzunbengin9@gmail.com>
2025-11-14 15:33:33 +0000

    adding

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - structure representing a dog
- * @name: type char *
- * @age: type float
- * @owner: type char *
- */
 struct dog
 {
 	char *name;
