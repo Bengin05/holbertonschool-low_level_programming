8bdea78244c1dc1b9f468fe91d88ac32b9237f04
Bengin05 <uzunbengin9@gmail.com>
2025-11-21 23:42:26 +0000

    adding function 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
+
+#include <stdarg.h>
+
+/**
+ * struct print - Struct associating a type char and function pointer
+ * @c: type to match
+ * @f: function to handle that type
+ */
+typedef struct print
+{
+    char *c;
+    void (*f)(va_list);
+} print_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
