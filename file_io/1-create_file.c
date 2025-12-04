00fddf06ac9604d0dd81ad8a94028da5e5879a3c
Bengin05 <uzunbengin9@gmail.com>
2025-12-04 09:56:27 +0000

    add the betty style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,23 +19,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written, text_length = 0;
-    if (filename == NULL)
-        return (-1);
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        while (text_content[text_length] != '\0')
-            text_length++;
-        bytes_written = write(fd, text_content, text_length);
-        if (bytes_written != text_length)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-    close(fd);
-    return (1);
+	int fd, bytes_written, text_length = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[text_length] != '\0')
+			text_length++;
+		bytes_written = write(fd, text_content, text_length);
+		if (bytes_written != text_length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
