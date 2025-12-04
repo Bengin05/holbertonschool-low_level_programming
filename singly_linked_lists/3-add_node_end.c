9abbd03de10de5be786da519e94d0ce5632d33e8
Bengin05 <uzunbengin9@gmail.com>
2025-12-04 09:16:04 +0000

    correction the function

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,49 +1,51 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_node_end - ajoute un nouveau nœud à la fin d'une liste chaînée
- * @head: double pointeur vers le premier élément de la liste
- * @str: chaîne de caractères à copier dans le nouveau nœud
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to duplicate and store in the new node
  *
- * Retour: l'adresse du nouveau nœud ajouté à la fin
- *		   ou NULL si l'allocation échoue
- *
- * La chaîne @str doit être dupliquée avec strdup.
+ * Return: Address of the new element, or NULL if it failed
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
+	size_t count = 0;
 	list_t *new;
-	list_t *temp;
+	list_t *temp = *head;
 
-	if (head == NULL || str == NULL)
-	{
-		return (NULL);
-	}
 	new = malloc(sizeof(list_t));
+
 	if (new == NULL)
-	{
-		return (NULL);
-	}
+	return (NULL);
+
 	new->str = strdup(str);
-	if (new->str ==  (NULL))
+
+	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	new->len = strlen(str);
-	new->next = NULL;
+
+	while (str[count] != '\0')
+		count++;
+
+	new->len = count;
+
 	if (*head == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
-	}
+{
+	*head = new;
+	new->next = NULL;
+	return (new);
+}
+
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+	new->next = NULL;
+
 	return (new);
 }
