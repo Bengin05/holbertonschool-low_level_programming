fb4e9d5c6c120a0d93361ad0b0022472ed9cf072
Bengin.05 <uzunbengin9@gmail.com>
2025-12-05 13:28:47 +0000

    correction for the function

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -10,12 +10,12 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index;
+    unsigned long int index;
 
-	if (key != NULL)
-		return (0);
+    if (key == NULL || size == 0)
+        return (0);
 
-	index = hash_djb2(key) % size;
+    index = hash_djb2(key) % size;
 
-	return (index);
+    return (index);
 }
